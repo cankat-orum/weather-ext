6d23d9ffe9a6e50adbb9f74dc44005a62ddca682
cankat-orum <jankatorumanp@gmail.com>
2023-12-01 13:18:50 +0300

    UPDATE fetchData.js
    - fetch icons and display it

--- fetchData.js
+++ fetchData.js
@@ -10,12 +10,23 @@ fetch(
     console.log(jsonData.main.feels_like);
     console.log(jsonData.weather[0].description);
 
-    document.getElementById("text_location").innerHTML = jsonData.name;
-    document.getElementById("text_location_country").innerHTML =
-      jsonData.sys.country;
-    document.getElementById("text_temp").innerHTML = jsonData.main.temp;
-    document.getElementById("text_feelslike").innerHTML =
-      jsonData.main.feels_like;
-    document.getElementById("text_desc").innerHTML =
-      jsonData.weather[0].description;
+    fetch(
+      `https://openweathermap.org/img/wn/${jsonData.weather[0].icon}@2x.png`
+    )
+      .then((res) => res.blob())
+      .then((result) => {
+        document.getElementById("text_location").innerHTML = jsonData.name;
+        document.getElementById("text_location_country").innerHTML =
+          jsonData.sys.country;
+        document.getElementById("text_temp").innerHTML = Math.round(
+          jsonData.main.temp
+        );
+        document.getElementById("text_feelslike").innerHTML =
+          "&nbsp" + Math.round(jsonData.main.feels_like);
+        document.getElementById("text_desc").innerHTML =
+          jsonData.weather[0].description;
+
+        const imageObjectURL = URL.createObjectURL(result);
+        document.getElementById("icon").src = imageObjectURL;
+      });
   });
